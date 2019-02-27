2eeee481120c4bb4c1c853efe5b5dced4b20e746
objcmate <objcmate@objcmates-MacBook-Pro.local>
2019-02-27 15:48:53 -0500

    rebuild

--- draftpick.py
+++ draftpick.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 from bs4 import BeautifulSoup
 
 
@@ -12,47 +12,69 @@ class DraftPick():
     def start_scrape(self):
         page = open(self.website)
         soup = BeautifulSoup(page, "html.parser")
+        ranks = []
+        imgs = []
+        names = []
+        schools = []
+        pos = []
+        hwght = []
+        raitings = []
+        complete = []
 
         # rankings
         athlete_list = soup.find_all('div', attrs={'class': 'primary'})
         for rank in athlete_list:
-            print(rank.string)
+            ranks.append(str(rank.string))
 
         # profile images
         athlete_list = soup.find_all('img', attrs={'class': 'jsonly'})
         for img in athlete_list:
-            print(img['src'])
+            imgs.append(str(img['src']))
 
         # names
         athlete_list = soup.find_all('a', attrs={'class': 'rankings-page__name-link'})
         for name in athlete_list:
-            print(name.string)
+            names.append(str(name.string))
 
         # school
         athlete_list = soup.find_all('span', attrs={'class': 'meta'})
         for school in athlete_list:
-            print(school.string)
+            schools.append(str(school.string))
 
         # positions
         athlete_list = soup.find_all('div', attrs={'class': 'position'})
-        for pos in athlete_list:
-            print(pos.string)
+        for position in athlete_list:
+            pos.append(str(position.string))
 
         # height -- weight
         athlete_list = soup.find_all('div', attrs={'class': 'metrics'})
         for htwt in athlete_list:
-            print(htwt.string)
+            hwght.append(str(htwt.string))
 
         # rating
         athlete_list = soup.find_all('span', attrs={'class': 'score'})
         for raiting in athlete_list:
-            print(raiting.string)
+            raitings.append(str(raiting.string))
+        
+        # combine all the data into one array
+        for key in ranks:
+            complete.append({
+                'rank': ranks[int(key) - 1],
+                'profile': imgs[int(key) - 1],
+                'name': names[int(key) - 1],
+                'school': schools[int(key) - 1],
+                'pos': pos[int(key) - 1],
+                'htwt': hwght[int(key) - 1],
+                'raiting': raitings[int(key) - 1]
+            })
+        
+        print(complete)
+            
 
 
 # my scrape
 my_scrape = DraftPick("2019_Top_Basketball_Recruits.html")   # due to site crashing, the page was exported to an HTML file
 my_scrape.start_scrape()
-
 
 
 ### WILL FIX ###
